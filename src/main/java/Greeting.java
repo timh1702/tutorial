bb432ccac5593517698b9f9ae118301200ce2491
timhildebrandt <17_Tim_H@web.de>
2018-03-21 14:24:44 +0100

    tutorial

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -9,7 +9,7 @@ public class Greeting {
 	 * Should write "Hello, <greetingRecipient>!" to stdout
 	 */
 	public void greet() {
-		System.out.println(greetingRecipient);
+		System.out.println("Hello, " + greetingRecipient + "!");
 	}
 
 	public static void main(String[] args) {
